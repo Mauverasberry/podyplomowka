75aaea7804cc3c4207a5bd79444f884f55b64e71
Aleksandra <aleksandra.kwast@gmail.com>
2024-11-29 11:01:49 +0100

    Add files via upload

---
+++ Sprawdzenie pełnoletności.py
@@ -0,0 +1,9 @@
+
+age = float(input('Podaj swój wiek:  '))
+
+if age >= 18:
+    print('Jesteś pełnoletni')
+else:
+    print('Jesteś niepełnoletni')
+
+    
---
+++ Wyświetlanie liczb nieparzystych.py
@@ -0,0 +1,6 @@
+
+
+for i in range(1,20,2): #argument step w funkcji range
+    print(i)
+
+    
